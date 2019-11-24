8c433a07b0121479f9c43c905dc8c07769d806da
Saihong Xiao <sxiao15@myseneca.ca>
2019-11-24 00:11:57 -0500

    add the database for username and password

--- server.js
+++ server.js
@@ -1,65 +1,84 @@
+
 const express = require("express");
 const multer = require("multer");
+const clientSessions = require("client-sessions");
 const bodyParser = require("body-parser");
 const path= require("path");
 // const dataService = require("./data-service.js");
 const fs = require('fs');
 const exphbs= require('express-handlebars');
-const HTTP_PORT = process.env.PORT || 8080;
+const dataServiceAuth = require("./data-service-auth.js");
 const app = express();
 
+const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(clientSessions({
+    cookieName: "session",
+    secret: "webassignment",
+    duration: 3*60*1000,
+    activeDuration: 2*60*1000
+}));
 
-
-
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(function(req,res,next){     
     let route = req.baseUrl + req.path;     
     app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");     
     next(); 
 }); 
-
+ 
 app.engine('.hbs', exphbs({ 
     extname: ".hbs",
     defaultLayout: "main",
     helpers:{
         navLink: function(url, options){     return '<li' +          
-            ((url == app.locals.activeRoute) ? ' class="active nav-item"' : ' class="nav-item"') +  
-            '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>'; } ,
-        
-            equal:(lvalue, rvalue, options)=>{
+            ((url == app.locals.activeRoute) ? ' class="active nav-item" ' : ' class="nav-item"') +  
+            '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>'; 
+        },   
+        equal:function(lvalue, rvalue, options){
             if(arguments.length <3)
                 throw new Error("Handlebars Helper equal needs 2 parameters");
             if (lvalue != rvalue){
                 return options.inverse(this);
-            }else{
+                
+            }else{              
                 return options.fn(this);
             }
         }
-
-       
     }
 }));
+        
+           
 app.set("view engine", ".hbs");
 
+function ensureLogin(req, res, next){
+
+    if(!req.session.user){
+        res.redirect("/login");
+    }else{
+        next();
+    }
+}
+
 app.get("/", function(req, res){
-   
     res.render("home");
 });
 
+app.get("/about", function(req, res){
+    res.render("about");
+});
 
-app.get("/images/add", function(req, res){
+
+
+app.get("/images/add", ensureLogin, function(req, res){
     res.render("addImage");
                
 });
-
-app.get("/signup", function(req, res){
-    res.render("signup");             
-});
-
-
 
 const storage = multer.diskStorage({
     destination: "./public/images/uploaded",
@@ -67,19 +86,22 @@ const storage = multer.diskStorage({
         cb(null, Date.now()+path.extname(file.originalname));
     }
 });
+const upload = multer({ storage:storage });
 
-const upload = multer({storage:storage});
+app.post("/images/add", ensureLogin, upload.single("imageFile"), function(req, res){
+    res.redirect('/images'); 
+} );
 
 
-
-app.get("/images",  function(req, res){  
+ app.get("/images", ensureLogin, function(req, res){  
       
-    fs.readdir("./public/images/uploaded", function(err, items){      
-        res.render("images", {data:items});
-   });
+     fs.readdir("./public/images/uploaded", function(err, items){      
+         res.render("images", {data:items});
+    });
 
-});
+ });
 
+ 
 
 
 
@@ -88,7 +110,6 @@ app.get("/login", function(req,res){
 });
 
 app.get("/register", function(req, res){
-    console.log("call the register");
     res.render("register");
 });
 
@@ -114,7 +135,7 @@ app.post("/login", (req, res) => {
         loginHistory: user.loginHistory
     }
 
-        res.redirect('/employees');
+        res.redirect('/images/add');
     }).catch((err) => {
       res.render("login", {errorMessage: err, userName: req.body.userName});
     });
@@ -122,14 +143,32 @@ app.post("/login", (req, res) => {
   
 
 
+
+
 app.get("/logout", function(req, res){
     req.session.reset();
     res.redirect("/");
 });
 
+app.get("/userHistory", ensureLogin, (req, res)=>{
+    res.render("userHistory");
+});
+
 app.use(function(req, res){
-    res.status(404).send("PAGE NOT FOUND!!!!!!!!!!!");
+    res.status(404).send("PAGE NOT FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11!!!!!!!!!!");
 });
 
 
-app.listen(HTTP_PORT, () => console.log(`app listening on port ${HTTP_PORT}!`));
+
+dataServiceAuth.initialize()
+      .then(function(mes){
+          console.log(mes);
+          app.listen(HTTP_PORT, function(){
+              console.log("app listening on: " + HTTP_PORT);
+          });
+      }).catch(function(err){
+          console.log("unable to start server: " + err);
+      });
+      
+
+
